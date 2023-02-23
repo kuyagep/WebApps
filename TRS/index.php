2400f366fa0ec53299edafffd719ca205e5507ae
Geperson Mamalias <125843365+kuyagep@users.noreply.github.com>
2023-02-23 21:29:17 +0800

    several revisions

--- TRS/index.php
+++ TRS/index.php
@@ -33,30 +33,38 @@
 
 
 <body class="bg-white">
+    <header class="header-global fixed-top bg-gradient-default">
+        <div class="m-2 ml-5">
+            <img src="./dashboard/assets/img/logo/deped-logo-white.png" width=500px" alt="">
+
+        </div>
+
+
+    </header>
 
     <!-- Main content -->
-    <div class="main-content">
+    <div class="main-content ">
         <!-- Login Form  -->
         <div id="login-box">
             <!-- Header -->
-            <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-3" id="login">
+            <div class="header  py-7 py-lg-8 pt-lg-3" id="login">
                 <div class="container">
                     <div class="header-body text-center mb-5">
                         <div class="row justify-content-center">
                             <div class="col-xl-5 col-lg-6 col-md-8 px-5">
-                                <img src="./dashboard/assets/img/logo/logo.png" alt="brand-logo" width="100px">
-                                <h1 class="text-white">Login Account</h1>
+                                <!-- <img src="./dashboard/assets/img/logo/logo.png" alt="brand-logo" width="100px"> -->
+                                <h1 class="text-dark">Login Account</h1>
                                 <!-- <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p> -->
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="separator separator-bottom separator-skew zindex-100">
+                <!-- <div class="separator separator-bottom separator-skew zindex-100">
                     <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
                         xmlns="http://www.w3.org/2000/svg">
                         <polygon class="fill-white" points="2560 0 2560 100 0 100"></polygon>
                     </svg>
-                </div>
+                </div> -->
             </div>
             <!-- Page content -->
             <div class="container mt--9 pb-5 text-gray">
@@ -87,16 +95,16 @@
                                             <input class="form-control" placeholder="Password" type="password"
                                                 name="password" id="password">
                                         </div>
-                                        
+
                                     </div>
                                     <div class="custom-control custom-control-alternative custom-checkbox">
-                                        <input class="custom-control-input" id="customCheckLogin" type="checkbox" >
+                                        <input class="custom-control-input" id="customCheckLogin" type="checkbox">
                                         <label class="custom-control-label" for=" customCheckLogin">
                                             <span>Remember me</span>
                                         </label>
                                     </div>
                                     <div class="text-center">
-                                        <button type="button" class="btn btn-primary my-4" id="login-btn">Sign
+                                        <button type="button" class="btn btn-primary my-4" id="login-btn"> <i class="fas fa-sign-in-alt mr-2"></i>Sign
                                             in</button>
                                     </div>
                                 </form>
@@ -104,10 +112,13 @@
                         </div>
                         <div class="row mt-3">
                             <div class="col-6">
-                                <a href="#" class="text-gray" id="forgot-link"><small>Forgot password?</small></a>
+                                <!-- <a href="#" class="text-gray" id="forgot-link"><small>Forgot password?</small></a> -->
+                                <a href="#" class="btn btn-md btn-secondary animate-up-2" id="forgot-link"></i> Forgot password?</a>
                             </div>
                             <div class="col-6 text-right">
-                                <a href="#" class="text-gray" id="register-link"><small>Register</small></a>
+                                <!-- <a href="#" class="text-gray" id="register-link"><small>Register</small></a> -->
+                                <a href="#" class="btn btn-md btn-primary animate-up-2" id="register-link"><i
+                                        class="fas fa-paper-plane mr-2"></i> Register</a>
                             </div>
                         </div>
                     </div>
@@ -119,24 +130,24 @@
         <!-- Register Form  -->
         <div id="register-box" style="display: none;">
             <!-- Header -->
-            <div class="header bg-gradient-primary  py-lg-8 pt-lg-3">
+            <div class="header   py-lg-8 pt-lg-3">
                 <div class="container">
                     <div class="header-body text-center mb-5">
                         <div class="row justify-content-center">
                             <div class="col-xl-5 col-lg-6 col-md-8 px-5">
-                                <img src="./dashboard/assets/img/logo/logo.png" alt="brand-logo" width="100px">
-                                <h1 class="text-white">Create an account</h1>
+                                <!-- <img src="./dashboard/assets/img/logo/logo.png" alt="brand-logo" width="100px"> -->
+                                <h1 class="text-dark">Create an account</h1>
                                 <!-- <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p> -->
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="separator separator-bottom separator-skew zindex-100">
+                <!-- <div class="separator separator-bottom separator-skew zindex-100">
                     <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
                         xmlns="http://www.w3.org/2000/svg">
                         <polygon class="fill-white" points="2560 0 2560 100 0 100"></polygon>
                     </svg>
-                </div>
+                </div> -->
             </div>
             <!-- Page content -->
             <div class="container mt--9 pb-5">
@@ -216,7 +227,8 @@
                                 <a href="#" class="text-gray"><small>Forgot password?</small></a>
                             </div> -->
                             <div class="col-12 text-right">
-                                <a href="#" class="text-gray" id="login-link"><small>Login</small></a>
+                                <!-- <a href="#" class="text-gray" id="login-link"><small>Login</small></a> -->
+                                <a href="#" class="btn btn-md btn-primary animate-up-2" id="login-link"> <i class="fas fa-sign-in-alt mr-2"></i></i></i>Login</a>
                             </div>
                         </div>
                     </div>
@@ -233,19 +245,19 @@
                     <div class="header-body text-center mb-5">
                         <div class="row justify-content-center">
                             <div class="col-xl-5 col-lg-6 col-md-8 px-5">
-                                <img src="./dashboard/assets/img/logo/logo.png" alt="brand-logo" width="100px">
+                                <!-- <img src="./dashboard/assets/img/logo/logo.png" alt="brand-logo" width="100px"> -->
                                 <h1 class="text-white">Reset Password</h1>
                                 <!-- <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p> -->
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="separator separator-bottom separator-skew zindex-100">
+                <!-- <div class="separator separator-bottom separator-skew zindex-100">
                     <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
                         xmlns="http://www.w3.org/2000/svg">
                         <polygon class="fill-white" points="2560 0 2560 100 0 100"></polygon>
                     </svg>
-                </div>
+                </div> -->
             </div>
             <!-- Page content -->
             <div class="container mt--9 pb-5 text-gray">
@@ -280,7 +292,8 @@
                                 <a href="#" class="text-gray"><small>Forgot password?</small></a>
                             </div> -->
                             <div class="col-12 text-right">
-                                <a href="#" class="text-gray" id="back-link"><small>Back</small></a>
+                                <!-- <a href="#" class="text-gray" id="back-link"><small>Back</small></a> -->
+                                <a href="#" class="btn btn-md btn-secondary animate-up-2" id="back-link">Back</a>
                             </div>
                         </div>
                     </div>
