6e67ed58ae223b3670e650266583d877c98cff3c
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-02-27 14:48:26 +0800

    update

--- TRS/index.php
+++ TRS/index.php
@@ -39,128 +39,14 @@
 
 
 <body class="bg-white">
-    <header class="header-global fixed-top bg-dark ">
-        <nav id="navbar-main "
-            class="navbar navbar-main navbar-expand-lg headroom py-lg-3 px-lg-6 navbar-light navbar-theme-default ">
-            <div class="container my-3">
-                <a class="navbar-brand @@logo_classes" href="../../index.html">
-                    <!-- <img class="navbar-brand-dark common" src="./dashboard/assets/img/brand/light.svg" height="35"
-                        alt="Logo light">
-                    <img class="navbar-brand-light common" src="./dashboard/assets/img/brand/dark.svg" height="35"
-                        alt="Logo dark"> -->
-                        <img class="navbar-brand-light common" src="./dashboard/assets/img/logo/deped-logo-white.png" height="70"
-                        alt="Logo dark">
-
-                </a>
-                <div class="navbar-collapse collapse" id="navbar_global">
-                    <div class="navbar-collapse-header">
-                        <div class="row">
-                            <div class="col-6 collapse-brand">
-                                <a href="../../index.html">
-                                    <img src="./dashboard/assets/img/brand/dark.svg" height="35" alt="Logo Impact">
-                                </a>
-                            </div>
-                            <div class="col-6 collapse-close">
-                                <a href="#navbar_global" role="button" class="fas fa-times" data-toggle="collapse"
-                                    data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false"
-                                    aria-label="Toggle navigation"></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- <ul class="navbar-nav navbar-nav-hover justify-content-center">
-                        <li class="nav-item">
-                            <a href="../../index.html" class="nav-link">Overview</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link" data-toggle="dropdown" aria-controls="pages_submenu"
-                                aria-expanded="false" aria-label="Toggle pages menu item">
-                                <span class="nav-link-inner-text">Front pages</span>
-                                <span class="fas fa-angle-down nav-link-arrow ml-2"></span>
-                            </a>
-                            <ul class="dropdown-menu" id="pages_submenu">
-                                <li><a class="dropdown-item" href="../pages/index.html">Landing</a></li>
-                                <li><a class="dropdown-item" href="../pages/about.html">About</a></li>
-                                <li><a class="dropdown-item" href="../pages/pricing.html">Pricing</a></li>
-                                <li><a class="dropdown-item" href="../pages/contact.html">Contact</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link" data-toggle="dropdown" aria-controls="pages_submenu"
-                                aria-expanded="false" aria-label="Toggle pages menu item">
-                                <span class="nav-link-inner-text">App pages</span>
-                                <span class="fas fa-angle-down nav-link-arrow ml-2"></span>
-                            </a>
-                            <ul class="dropdown-menu" id="pages_submenu">
-                                <li><a class="dropdown-item"
-                                        href="../../dashboard/pages/dashboards/dashboard.html">Dashboard</a></li>
-                                <li><a class="dropdown-item" href="../../dashboard/pages/examples/login.html">Login</a>
-                                </li>
-                                <li><a class="dropdown-item"
-                                        href="../../dashboard/pages/examples/register.html">Register</a></li>
-                                <li><a class="dropdown-item"
-                                        href="../../dashboard/pages/examples/profile.html">Profile</a></li>
-                                <li><a class="dropdown-item" href="../../dashboard/pages/tables/tables.html">Tables</a>
-                                </li>
-                                <li><a class="dropdown-item" href="../../dashboard/pages/maps/google.html">Maps</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" aria-expanded="false" data-toggle="dropdown">
-                                <span class="nav-link-inner-text mr-1">Support</span>
-                                <i class="fas fa-angle-down nav-link-arrow"></i>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-lg">
-                                <div class="col-auto px-0" data-dropdown-content>
-                                    <div class="list-group list-group-flush">
-                                        <a href="https://demos.creative-tim.com/impact-design-system-pro/docs/getting-started/overview/"
-                                            class="list-group-item list-group-item-action d-flex align-items-center p-0 py-3 px-lg-4">
-                                            <span class="icon icon-sm icon-secondary"><i
-                                                    class="fas fa-file-alt"></i></span>
-                                            <div class="ml-4">
-                                                <span class="text-dark d-block">Documentation<span
-                                                        class="badge badge-sm badge-secondary ml-2">v1.0</span></span>
-                                                <span class="small">Examples and guides</span>
-                                            </div>
-                                        </a>
-                                        <a href="https://github.com/creativetimofficial/impact-design-system/issues"
-                                            target="_blank"
-                                            class="list-group-item list-group-item-action d-flex align-items-center p-0 py-3 px-lg-4">
-                                            <span class="icon icon-sm icon-primary"><i
-                                                    class="fas fa-microphone-alt"></i></span>
-                                            <div class="ml-4">
-                                                <span class="text-dark d-block">Support</span>
-                                                <span class="small">Found a bug? Create an issue!</span>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    </ul> -->
-                </div>
-                <!-- <div class="d-none d-lg-block @@cta_button_classes">
-                    <a href="https://demos.creative-tim.com/impact-design-system-pro/docs/dashboard/alerts/"
-                        class="btn btn-md btn-docs btn-outline-white animate-up-2 mr-3"><i
-                            class="fas fa-th-large mr-2"></i> Components</a>
-                    <a href="https://www.creative-tim.com/product/impact-design-system-pro" target="_blank"
-                        class="btn btn-md btn-secondary animate-up-2"><i class="fas fa-paper-plane mr-2"></i> Upgrade to
-                        Pro</a>
-                </div> -->
-                <div class="d-flex d-lg-none align-items-center">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global"
-                        aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation"><span
-                            class="navbar-toggler-icon"></span></button>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php include("header.php") ?>
 
     <!-- Main content -->
     <div class="main-content ">
         <!-- Login Form  -->
         <div id="login-box">
             <!-- Header -->
-            <div class="header  py-7 py-lg-8 pt-lg-3" id="login">
+            <div class="header  py-8 py-lg-9 pt-lg-3" id="login">
                 <div class="container">
                     <div class="header-body text-center mb-5">
                         <div class="row justify-content-center">
@@ -183,10 +69,10 @@
             <div class="container mt--9 pb-5 text-gray">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-7">
-                        <div class="card bg-secondary border border-soft mb-0">
+                        <div class="card bg-dark border border-soft mb-0">
 
                             <div class="card-body px-lg-5 py-lg-5">
-                                <div class="text-center mb-4">
+                                <div class="text-center mb-4 text-white">
                                     <small>Please enter your Email and Password.</small>
                                 </div>
                                 <form role="form">
@@ -211,9 +97,9 @@
 
                                     </div>
                                     <div class="custom-control custom-control-alternative custom-checkbox">
-                                        <input class="custom-control-input" id="customCheckLogin" type="checkbox">
+                                        <input class="custom-control-input" id="customCheckLogin" type="checkbox" checked>
                                         <label class="custom-control-label" for=" customCheckLogin">
-                                            <span>Remember me</span>
+                                            <span class="text-white">Remember me</span>
                                         </label>
                                     </div>
                                     <div class="text-center">
@@ -245,7 +131,7 @@
         <!-- Register Form  -->
         <div id="register-box" style="display: none;">
             <!-- Header -->
-            <div class="header   py-lg-8 pt-lg-3">
+            <div class="header  py-8 py-lg-9 pt-lg-3">
                 <div class="container">
                     <div class="header-body text-center mb-5">
                         <div class="row justify-content-center">
@@ -335,7 +221,8 @@
                                         </div>
                                     </div>
                                     <div class="text-center">
-                                        <input type="button" class="btn btn-primary mt-4" value="Sign Up" id="register-btn">
+                                        <input type="button" class="btn btn-primary mt-4" value="Sign Up"
+                                            id="register-btn">
                                     </div>
                                 </form>
                             </div>
@@ -359,13 +246,13 @@
         <!-- Forgot Password  -->
         <div id="forgot-box" style="display: none;">
             <!-- Header -->
-            <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
+            <div class="header  py-7 py-lg-8 pt-lg-9">
                 <div class="container">
                     <div class="header-body text-center mb-5">
                         <div class="row justify-content-center">
                             <div class="col-xl-5 col-lg-6 col-md-8 px-5">
                                 <!-- <img src="./dashboard/assets/img/logo/logo.png" alt="brand-logo" width="100px"> -->
-                                <h1 class="text-white">Reset Password</h1>
+                                <h1 class="text-dark">Reset Password</h1>
                                 <!-- <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p> -->
                             </div>
                         </div>
@@ -439,20 +326,20 @@
     <script src="./dashboard/assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
     <script src="./dashboard/assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
 
-     <!-- Core -->
-<script src="./vendor/headroom.js/dist/headroom.min.js"></script>
+    <!-- Core -->
+    <script src="./vendor/headroom.js/dist/headroom.min.js"></script>
 
-<!-- Vendor JS -->
-<script src="./vendor/onscreen/dist/on-screen.umd.min.js"></script>
-<script src="./vendor/waypoints/lib/jquery.waypoints.min.js"></script>
-<script src="./vendor/jarallax/dist/jarallax.min.js"></script>
-<script src="./vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+    <!-- Vendor JS -->
+    <script src="./vendor/onscreen/dist/on-screen.umd.min.js"></script>
+    <script src="./vendor/waypoints/lib/jquery.waypoints.min.js"></script>
+    <script src="./vendor/jarallax/dist/jarallax.min.js"></script>
+    <script src="./vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
 
     <!-- Impact JS -->
-<script src="./front/assets/js/front.js"></script>
+    <script src="./front/assets/js/front.js"></script>
 
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js"></script> -->
@@ -476,20 +363,20 @@
         });
 
         //Register Ajax Request
-        $("#register-btn").click(function(e){
-            if($("#register-form")[0].checkValidity()){
+        $("#register-btn").click(function(e) {
+            if ($("#register-form")[0].checkValidity()) {
                 e.preventDefault();
                 $("#register-btn").val('Please Wait...');
-                if($("#rpassword").val() != $("#cpassword").val()){
+                if ($("#rpassword").val() != $("#cpassword").val()) {
                     $("#passError").text("* Password did not matched!");
                     $("#register-btn").val('Sign Up');
-                }else{
+                } else {
                     $("#passError").text("");
                     $.ajax({
                         url: 'assets/php/action.php',
                         method: 'post',
                         data: $("#register-form").serialize() + '&action=register',
-                        success: function(response){
+                        success: function(response) {
                             console.log(response);
                             $("#register-btn").val('Sign Up');
                         }
