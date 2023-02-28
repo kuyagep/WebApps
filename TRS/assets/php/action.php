5ac83388461db959970c52c05febb6b3bd55690a
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-02-28 13:06:37 +0800

    Update action.php

--- TRS/assets/php/action.php
+++ TRS/assets/php/action.php
@@ -1,5 +1,21 @@
 <?php
+
+    require_once 'auth.php';
+    $user = new Auth();
     if(isset($_POST['action']) && $_POST['action'] == "register"){
-        print_r($_POST);
+        $name = $user->check_input($_POST['name']);
+        $email = $user->check_input($_POST['email']);
+        $password = $user->check_input($_POST['password']);
+        $hash_pass = password_hash($password, PASSWORD_DEFAULT);
+
+        if($user->user_exist($email)){
+            echo $user->showMessage('warning','This E-Mail is already registered!');
+        }else{
+            if($user->register($name,$email,$password)){
+                echo 'register';
+                $_SESSION['user'] = $email;
+            }
+        }
+
     }
 ?>
