9664d158fd809f2f33fada3a2db9de72dfb4d888
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-02-28 12:54:21 +0800

    modify login system

--- TRS/login.php
+++ TRS/login.php
@@ -69,7 +69,7 @@
             <div class="container mt--9 pb-5 text-gray">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-7">
-                        <div class="card bg-gray border rounded-4 border-soft mb-0 ">
+                        <div class="card bg-gray border rounded-4 border-soft mb-0 shadow">
 
                             <div class="card-body px-lg-5 py-lg-5">
                                 <div class="text-center mb-4">
@@ -99,14 +99,11 @@
                                     <div class="row mt-3">
                                         <div class="col-6">
                                             <!-- <a href="#" class="text-gray" id="forgot-link"><small>Forgot password?</small></a> -->
-                                            <div class="custom-control custom-control-alternative custom-checkbox">
-                                                <input class="custom-control-input" id="customCheckLogin"
-                                                    type="checkbox" value="remember_me">
-                                                <!-- <input class="custom-control-input" type="checkbox" value="remember-me"> -->
-                                                <label class="custom-control-label" for="customCheckLogin">
-                                                    <span>Remember me</span>
-                                                </label>
-                                            </div>
+                                            <input class="custom-control-input" id="customCheckRegister" type="checkbox" value="remember_me">
+                                        <!-- <input class="custom-control-input" type="checkbox" value="remember-me"> -->
+                                        <label class="custom-control-label" for=" customCheckLogin">
+                                            <span >Remember me</span>
+                                        </label>
 
                                         </div>
                                         <div class="col-6 text-right">
@@ -115,6 +112,13 @@
                                                     password?</small></a>
 
                                         </div>
+                                    </div>
+                                    <div class="custom-control custom-control-alternative custom-checkbox">
+                                        <input class="custom-control-input" id="customCheckRegister" type="checkbox" value="remember_me">
+                                        <!-- <input class="custom-control-input" type="checkbox" value="remember-me"> -->
+                                        <label class="custom-control-label" for=" customCheckLogin">
+                                            <span >Remember me</span>
+                                        </label>
                                     </div>
                                     <div class="text-center">
                                         <button type="button" class="w-100  btn btn-lg  btn-primary mt-3 animate-up-2"
@@ -143,122 +147,7 @@
         </div>
         <!-- Login Form End  -->
 
-        <!-- Register Form  -->
-        <div id="register-box" style="display: none;">
-            <!-- Header -->
-            <div class="header  py-8 py-lg-9 pt-lg-3">
-                <div class="container">
-                    <div class="header-body text-center mb-3">
-                        <div class="row justify-content-center">
-                            <div class="col-xl-5 col-lg-6 col-md-8 px-5">
-                                <!-- <img src="./dashboard/assets/img/logo/logo.png" alt="brand-logo" width="100px"> -->
-                                <h1 class="text-dark">Create an account</h1>
-                                <!-- <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p> -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- <div class="separator separator-bottom separator-skew zindex-100">
-                    <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <polygon class="fill-white" points="2560 0 2560 100 0 100"></polygon>
-                    </svg>
-                </div> -->
-            </div>
-            <!-- Page content -->
-            <div class="container mt--9 pb-5">
-                <!-- Table -->
-                <div class="row justify-content-center">
-                    <div class="col-lg-6 col-md-8">
-                        <div class="card bg-gray border border-soft">
-
-                            <div class="card-body px-lg-5 py-lg-5">
-                                <div class="text-center text-muted mb-4 text-white">
-                                    <small>Sign up with credentials</small>
-                                </div>
-                                <form role="form" id="register-form">
-                                    <div class="form-group">
-                                        <div class="input-group input-group-merge input-group-alternative mb-3">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
-                                            </div>
-                                            <input class="form-control" placeholder="Name" type="text" id="name"
-                                                name="name" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group input-group-merge input-group-alternative mb-3">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                            </div>
-                                            <input class="form-control" placeholder="Email" type="email" id="remail"
-                                                name="remail" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group input-group-merge input-group-alternative">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i
-                                                        class="ni ni-lock-circle-open"></i></span>
-                                            </div>
-                                            <input class="form-control" placeholder="Password" type="password"
-                                                id="rpassword" name="rpassword" minlength="5" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group input-group-merge input-group-alternative">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i
-                                                        class="ni ni-lock-circle-open"></i></span>
-                                            </div>
-                                            <input class="form-control" placeholder="Confirm-Password" type="password"
-                                                id="cpassword" name="cpassword" minlength="5" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="text-danger font-weight-bold" id="passError"></div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div id="passError" class="text-danger font-weight-bolder"></div>
-                                    </div>
-                                    <!-- <div class="text-muted font-italic"><small>password strength: <span
-                                                class="text-success font-weight-700">strong</span></small></div> -->
-                                    <div class="row my-4">
-                                        <div class="col-12">
-                                            <!-- <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small> -->
-                                            <div class="custom-control custom-control-alternative custom-checkbox">
-                                                <input class="custom-control-input " id="customCheckRegister"
-                                                    type="checkbox">
-                                                <label class="custom-control-label" for="customCheckRegister">
-                                                    <span class="text-white">I agree with the <a href="#!">Privacy
-                                                            Policy</a></span>
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="text-center">
-                                        <input type="button" class="w-100 mb-2 btn btn-lg rounded-3 btn-primary mt-4"
-                                            value="Register" id="register-btn">
-
-                                    </div>
-                                    <hr class="my-4">
-                                    <div class="text-center">
-                                        <button type="button"
-                                            class="w-100 mb-2 btn btn-lg  btn-white  animate-up-2 text-success"
-                                            id="login-link"> <i class="fas fa-sign-in-alt mr-2 "></i>Login</button>
-                                        <!-- <button type="button" class="btn btn-primary my-4" id="login-btn"> <i
-                                                class="fas fa-sign-in-alt mr-2"></i>Sign
-                                            in</button> -->
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Register Form End  -->
+     
 
         <!-- Forgot Password  -->
         <div id="forgot-box" style="display: none;">
@@ -303,20 +192,18 @@
                                         </div>
                                     </div>
                                     <div class="text-center ">
-                                        <button type="button " id="forgot-btn"
-                                            class="w-100 mb-2 btn btn-lg btn-primary mt-4">Reset
+                                        <button type="button " id="forgot-btn" class="w-100 mb-2 btn btn-lg btn-primary mt-4">Reset
                                             Password</button>
                                     </div>
                                     <hr>
-
+                                    
                                 </form>
                                 <div class="text-center ">
-                                    <button type="button " class="w-100 mb-2 btn btn-lg btn-white "
-                                        id="back-link">Back</button>
-                                </div>
+                                        <button type="button " class="w-100 mb-2 btn btn-lg btn-white " id="back-link">Back</button>
+                                    </div>
                             </div>
                         </div>
-
+                        
                     </div>
                 </div>
             </div>
@@ -356,8 +243,8 @@
     <!-- Impact JS -->
     <script src="./front/assets/js/front.js"></script>
 
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script> -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js"></script> -->
     <script>
     $(document).ready(function() {
         $("#register-link").click(function() {
